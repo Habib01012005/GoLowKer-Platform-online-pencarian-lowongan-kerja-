d739cf678b3c757f9ea2b8fb8ecfd8460f9ac635
Habib01012005 <ahmadhabibalbukhori@gmail.com>
2025-05-04 22:52:28 +0800

    Initial commit - add Laravel project files

---
+++ database/migrations/2025_05_04_134312_create_jobseekers_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('jobseekers', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('password');
+        
+            // Data tambahan user
+            $table->string('phone')->nullable();
+            $table->text('address')->nullable();
+            $table->string('resume')->nullable(); // file CV, jika ada
+        
+            $table->timestamps();
+        });
+        
+    }
+    public function down(): void
+    {
+        Schema::dropIfExists('jobseekers');
+    }
+};
---
+++ database/migrations/2025_05_04_134337_create_companies_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    
+    public function up(): void
+    {
+        Schema::create('companies', function (Blueprint $table) {
+            $table->id();
+            $table->string('company_name');
+            $table->string('email')->unique();
+            $table->string('password');
+        
+            // Data tambahan perusahaan
+            $table->string('phone')->nullable();
+            $table->text('company_description')->nullable();
+            $table->string('website')->nullable();
+            $table->text('address')->nullable();
+        
+            $table->timestamps();
+        });
+        
+    }
+    public function down(): void
+    {
+        Schema::dropIfExists('companies');
+    }
+};
